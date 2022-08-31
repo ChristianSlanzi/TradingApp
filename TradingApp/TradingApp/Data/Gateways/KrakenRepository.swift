a5cb71e7897225cdf49b6ef0dfb73d4318a7079b
Christian Slanzi <christian.slanzi@gmail.com>
2022-08-31 20:00:51 +0200

    created the repository pattern

---
+++ TradingApp/TradingApp/Data/Gateways/KrakenRepository.swift
@@ -0,0 +1,26 @@
+//
+//  KrakenRepository.swift
+//  TradingApp
+//
+//  Created by Christian Slanzi on 31.08.22.
+//
+
+import Foundation
+import Combine
+
+public protocol KrakenRepositoryType { // a.k.a. gateway
+    func getTradableAssetsPairs() -> AnyPublisher<[String: TradingAssetPair], Error>
+}
+
+public struct KrakenRepository: KrakenRepositoryType {
+    let apiClient: KrakenAPIType
+    
+    public func getTradableAssetsPairs() -> AnyPublisher<[String : TradingAssetPair], Error> {
+        return apiClient.getTradableAssetsPairs()
+    }
+}
+
+/// REPOSITORY PATTERN
+///
+/// 1: common interface to access to different sources
+/// 2: compose multiple sources with the same interface
