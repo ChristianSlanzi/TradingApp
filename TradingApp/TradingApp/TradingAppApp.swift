cdba36157a2db09a16eb84103d023fd296d4ec68
Christian Slanzi <christian.slanzi@gmail.com>
2022-08-31 19:00:17 +0200

    Initial commit

---
+++ TradingApp/TradingApp/TradingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingAppApp.swift
+//  TradingApp
+//
+//  Created by Christian Slanzi on 31.08.22.
+//
+
+import SwiftUI
+
+@main
+struct TradingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
